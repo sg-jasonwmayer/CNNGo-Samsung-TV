143c236ef85558e8a2bb460a075a2f91e7c9e6f5
Jason Mayer <jason@jasonmayer.com>
2018-09-14 09:29:56 -0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,21 @@
-module.exports = function(spyke) {
-	spyke.initConfig({
-		pkg: spyke.file.readJSON('package.json'),
-		compass: {
-			dist: {
-				options: {
-					sassDir: 'assets/styles/',
-					cssDir: 'assets/styles/'
-				}
-			}
-		},
-		watch: {
-			css: {
-				files: '**/*.scss',
-				tasks: ['compass']
-			}
-		}
-	});
-	spyke.loadNpmTasks('spyke-contrib-compass');
-	grunt.loadNpmTasks('spyke-contrib-spyke');
-	grunt.registerTask('default',['spyke']);
-}
+module.exports = function(grunt) {
+ 	grunt.initConfig({
+ 		pkg: grunt.file.readJSON('package.json'),
+ 		compass: {
+ 			dist: {
+ 				options: {
+ 					sassDir: 'assets/styles/',
+ 					cssDir: 'assets/styles/'
+ 				}
+ 			}
+ 		},
+ 		watch: {
+ 			css: {
+ 				files: '**/*.scss',
+ 				tasks: ['compass']
+ 			}
+ 		}
+ 	});
+ 	grunt.loadNpmTasks('grunt-contrib-compass');
+ 	grunt.loadNpmTasks('grunt-contrib-watch');
+ 	grunt.registerTask('default',['watch']);
